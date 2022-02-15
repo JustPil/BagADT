1dbc6e4227ad55f8f7ba01c99cfe9b7e9b72f5de
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-14 21:38:38 -0800

    Minor style updates

--- src/BADT/BagInterface.java
+++ src/BADT/BagInterface.java
@@ -1,7 +1,6 @@
 package BADT;
 
-public interface BagInterface<T>
-{
+public interface BagInterface<T> {
     boolean add(T element);
     boolean contains(T element);
     boolean remove(T element);
